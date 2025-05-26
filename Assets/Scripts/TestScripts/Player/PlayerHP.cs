1bd0b9d94a25f3f90b4337ebafeddcf9b1b49e09
Fhunii <huni.fanta.4288@gmail.com>
2025-05-27 08:49:34 +0900

    feat: アタッチ前、HP表示機能の追加

---
+++ Assets/Scripts/TestScripts/Player/PlayerHP.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UIを使用する際は宣言が必要
+ 
+public class PlayerHP : MonoBehaviour
+{
+    [SerializeField] StatusData statusdata;
+    public Slider hpSlider;
+    float HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hpBar != null)
+        {
+            hpBar.maxValue= statusdata.MAXHP;
+            hpBar.value = statusdata.MAXHP;
+        }
+        HP = statusdata.MAXHP;
+    }
+}
