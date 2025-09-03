d973284d8410b4ef7edf7d872fa43b635e0c374f
Fhunii <huni.fanta.4288@gmail.com>
2025-09-03 12:24:52 +0900

    保存

--- Assets/Scripts/TestScripts/Level/LevelUpPanel.cs
+++ Assets/Scripts/TestScripts/Level/LevelUpPanel.cs
@@ -44,21 +44,23 @@ public class LevelUpPanel : MonoBehaviour
         Debug.Log(Imagename.sprite.name);
         if (Imagename.sprite.name == "ItemPanel1_0")
         {
-            //ドローンの生成
             var Drone = Instantiate(drone, transform.position, transform.rotation);
-            Debug.Log("ドローンを選択");
+            Debug.Log("乱を選択");
+            //ソースイメージを変更
+            
+
         }
 
 	    if (Imagename.sprite.name == "ItemPanel2_0")
         {
-            Debug.Log("パンチを選択");
+            Debug.Log("制を選択");
 	        runtimeStatus.AddATK(1);
             Debug.Log("ATK:"+runtimeStatus.ATK);
         }
 
 	    if (Imagename.sprite.name == "ItemPanel3_0")
         {
-            Debug.Log("聖水を選択");
+            Debug.Log("芸を選択");
         }
 
         Time.timeScale = 1;
